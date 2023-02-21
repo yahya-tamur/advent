0390cdb0aae93af0b27c4c9a63e12a62d603f076
kerem <77764573+yahya-tamur@users.noreply.github.com>
2023-02-20 20:02:33 -0800

    did a1

---
+++ a1/src/main.rs
@@ -0,0 +1,27 @@
+use std::cmp::Reverse;
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
+use std::collections::BinaryHeap;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let file = File::open("input.txt")?;
+    let mut reader = BufReader::new(file);
+    let mut heap = BinaryHeap::from([Reverse(0);3]);
+    let mut acc: i32 = 0;
+
+    for line in &mut reader.lines().map(|a| a.unwrap()) {
+      if line.is_empty() {
+        heap.push(Reverse(acc));
+        heap.pop();
+        acc = 0;
+      } else {
+        acc += line.parse::<i32>()?;
+      }
+    }
+  heap.push(Reverse(acc));
+  heap.pop();
+  let v: Vec<i32> = heap.into_sorted_vec().into_iter().map(|a| a.0).collect();
+  println!("{:?}", v);
+  println!("{:?}", v.iter().sum::<i32>());
+  Ok(())
+}
