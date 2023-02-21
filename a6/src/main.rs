1ceeeb674522e3806fc73558d9d1362e080b8536
kerem <77764573+yahya-tamur@users.noreply.github.com>
2023-02-21 00:39:06 -0800

    did 6

---
+++ a6/src/main.rs
@@ -0,0 +1,34 @@
+use std::collections::LinkedList;
+use std::fs;
+
+const DIST: u8 = 14;
+
+fn bitcount(mut u : u32) -> u32 {
+  let mut count = 0;
+  while u != 0 {
+    count += 1;
+    u = u & (u - 1);
+  }
+  count
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+  let input = fs::read_to_string("input.txt").unwrap();
+  let bytes = input.as_bytes();
+  let mut mask = 0u32;
+  let mut queue: LinkedList<u8> = LinkedList::new();
+  for (i, b) in bytes.iter().enumerate() {
+    let b = *b - b'a';
+    mask ^= 1 << b;
+    queue.push_back(b);
+    if queue.len() == DIST.into() {
+      if bitcount(mask) == DIST.into() {
+        println!("{}", i+1);
+        return Ok(());
+      }
+      let q = queue.pop_front().unwrap();
+      mask ^= 1 << q;
+    }
+  }
+  Ok(())
+}
