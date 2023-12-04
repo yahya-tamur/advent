7f6f4ee01589f7f7a488076ffe1c394a72849d16
kerem <77764573+yahya-tamur@users.noreply.github.com>
2023-12-04 15:59:22 -0800

    2023 3,4

---
+++ python/a2023/3.py
@@ -0,0 +1,46 @@
+from common import get_problem
+from collections import defaultdict
+
+
+s = [list(line) + ['.'] for line in get_problem(2023, 3).split('\n') if line]
+s += [['.']*len(s[0])]
+
+partsum = 0
+
+gears = defaultdict(list)
+
+for row, line in enumerate(s):
+    i = 0
+    while i < len(line):
+        if line[i].isdigit():
+            j = i
+            while line[j].isdigit():
+                j += 1
+            num = int(''.join(line[i:j]))
+
+            adj = [(row,i-1),(row,j)] + [(row-1,k) for k in range(i-1,j+1)] + \
+                    [(row+1,k) for k in range(i-1,j+1)]
+
+            for (i_, j_) in adj:
+                if s[i_][j_] != '.':
+                    gears[(i_,j_)] = gears[(i_,j_)] + [num]
+
+            if any([s[i_][j_] != '.' for (i_, j_) in adj]):
+                partsum += num
+
+            i = j
+        i += 1
+
+print(f'part 1: {partsum}')
+
+gearsum = 0
+for gear in gears.values():
+    if len(gear) == 2:
+        gearsum += gear[0] * gear[1]
+
+print(f'part 2: {gearsum}')
+
+#for line in s:
+    #for c in line:
+        #print(c,end='')
+    #print()
---
+++ python/a2023/4.py
@@ -0,0 +1,19 @@
+from common import get_problem_lines
+
+s = get_problem_lines(2023, 4)
+
+part1 = 0
+part2 = [1 for _ in s]
+
+for (i, ln) in enumerate(s):
+    ln = ln.split(':')[1].strip()
+    (l, r) = ln.split('|')
+    l = {s for s in l.strip().split(' ') if s}
+    n = len([x for x in r.split(' ') if x in l])
+    if n:
+        for k in range(i+1,min(i+n+1,len(s))):
+            part2[k] += part2[i]
+        part1 += 2 ** (n-1)
+
+print(f"part 1: {part1}")
+print(f"part 2: {sum(part2)}")
