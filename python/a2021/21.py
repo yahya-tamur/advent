24124c0d4d410863d4589e4d89cb8a45c9a1a149
kerem <77764573+yahya-tamur@users.noreply.github.com>
2023-12-05 02:54:32 -0800

    2021 21 22

---
+++ python/a2021/21.py
@@ -0,0 +1,64 @@
+from common import get_problem_lines
+start = [ int(line.split(':')[1]) - 1 for line in get_problem_lines(2021,21) ]
+# player = [3, 7]
+
+player = [start[0], start[1]]
+scores = [0,0]
+
+die = 1
+rolled = 0
+
+def get_die():
+    global die, rolled
+    rolled += 1
+    ans = die
+    die += 1
+    if die == 101:
+        die = 1
+    return ans
+
+
+while True:
+#for _ in range(3):
+    player[0] = (player[0] + get_die() + get_die() + get_die()) % 10
+    scores[0] += player[0] + 1
+    if scores[0] >= 1000:
+        break
+    player[1] = (player[1] + get_die() + get_die() + get_die()) % 10
+    scores[1] += player[1] + 1
+    if scores[1] >= 1000:
+        break
+
+    if scores[0] >= 1000:
+        break
+
+print(f"part 1: {min(scores)*rolled}")
+
+dice_dist = [i+j+k for i in range(1,4) for j in range(1,4) for k in range(1,4)]
+dice_dist = [(i,len([d for d in dice_dist if d == i])) for i in range(3,10)]
+
+# max number of states: 10*10*21*21*2 : manageable
+states = dict() # (p1, p2, s1, s2,turn) -> [p1win, p2win]
+def get_wins(p1, p2, s1, s2, turn):
+    if s1 >= 21:
+        return [1,0]
+    if s2 >= 21:
+        return [0,1]
+    if (wl := states.get((p1,p2,s1,s2,turn))) is not None:
+        return wl
+    w, l = 0, 0
+    for (num, unvs) in dice_dist:
+        if turn == 1:
+            p1_ = (p1 + num) % 10
+            (w_,l_) = get_wins(p1_, p2, s1 + p1_ + 1, s2, 2)
+            w += w_*unvs
+            l += l_*unvs
+        else:
+            p2_ = (p2 + num) % 10
+            (w_,l_) = get_wins(p1, p2_, s1, s2 + p2_ + 1, 1)
+            w += w_*unvs
+            l += l_*unvs
+    states[(p1,p2,s1,s2,turn)] = (w,l)
+    return (w,l)
+
+print(f"part 2: {max(get_wins(start[0],start[1],0,0,1))}")
---
+++ python/a2021/22.py
@@ -0,0 +1,50 @@
+#BOUNDARIES:
+# 4..5
+# infer cubes:
+# [-inf,4], [4,5], [6,inf]
+# if you then see [9,10] create:
+
+# OK JUST DO WHAT YOU'RE ACTUALLY SUPPOSED TO DO.
+
+# cubes = <six nums>
+from common import get_problem_lines
+
+# returns list of cubes.
+def delete_cube(a, b):
+    if a[0] > b[1] or a[1] < b[0] or a[2] > b[3] or a[3] < b[2] \
+            or a[4] > b[5] or a[5] < b[4]:
+        return [a]
+    ans = list()
+    for n in range(3):
+        if a[2*n] < b[2*n] and b[2*n] <= a[2*n+1]:
+            ans.append(a.copy())
+            ans[-1][2*n+1] = b[2*n] - 1
+            a[2*n] = b[2*n]
+        if a[2*n] <= b[2*n+1] and b[2*n+1] < a[2*n+1]:
+            ans.append(a.copy())
+            ans[-1][2*n] = b[2*n+1]+1
+            a[2*n+1] = b[2*n+1]
+    return ans
+
+cubes = list()
+
+def total_vol(cs):
+    return sum((x2-x1+1)*(y2-y1+1)*(z2-z1+1) for \
+            (x1, x2, y1, y2, z1, z2) in cs)
+
+part1done = False
+
+s = get_problem_lines(2021, 22)
+for line in s:
+    line = line.replace(" x=","|").replace("..","|").replace(",y=","|").replace(",z=","|")
+    if (not part1done) and len(line) > 30:
+        print(f"part 1: {total_vol(cubes)}")
+        part1done = True
+    newcube = [int(x) for x in line.split("|")[1:]]
+    cubes_ = list()
+    for cube in cubes:
+        cubes_.extend(delete_cube(cube, newcube))
+    if line[1] == 'n':
+        cubes_.append(newcube)
+    cubes = cubes_
+print(f"part 2: {total_vol(cubes)}")
