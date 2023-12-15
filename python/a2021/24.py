53da805e1baa93588f82b67528be85ad341d50db
kerem <77764573+yahya-tamur@users.noreply.github.com>
2023-12-14 16:37:42 -0800

    finished 2021! only thing: maybe parallelize 23?

---
+++ python/a2021/24.py
@@ -0,0 +1,26 @@
+from common import get_problem_lines
+
+lines = [line.split(' ') for line in get_problem_lines()]
+sections = [lines[i:i+18] for i in range(0,len(lines),18)]
+
+stack = []
+constraints = [] # (i,n,j) means w[i] + n = w[j] i < j.
+
+for (i, section) in enumerate(sections):
+    if section[4][2] == "1":
+        stack.append((i,section[15][2]))
+    else:
+        (index, num) = stack.pop()
+        constraints.append((index, int(num) + int(section[5][2]), i))
+
+ans1, ans2 = ["9" for _ in range(14)], ["1" for _ in range(14)]
+for (i, n, j) in constraints:
+    if n > 0:
+        ans1[i] = str(9 - n)
+        ans2[j] = str(1+n)
+    else:
+        ans1[j] = str(9 + n)
+        ans2[i] = str(1-n)
+
+print(f"part 1: {''.join(ans1)}")
+print(f"part 2: {''.join(ans2)}")
---
+++ python/a2021/25.py
@@ -0,0 +1,52 @@
+from common import get_problem_lines
+from copy import deepcopy
+
+map = [list(line) for line in get_problem_lines(file="input")]
+map_ = [list(line) for line in get_problem_lines(file="input")]
+tmp = [list(line) for line in get_problem_lines(file="input")]
+
+def step(map, map_, tmp):
+    for i in range(len(map)):
+        for j in range(len(map[0])):
+            tmp[i][j] = map[i][j]
+    for i in range(len(map)):
+        for j in range(len(map[0])):
+            if map[i][j] == '.':
+                if map[i][j-1] == '>':
+                    tmp[i][j] = '>'
+                    tmp[i][j-1] = '.'
+    for i in range(len(map)):
+        for j in range(len(map[0])):
+            map_[i][j] = tmp[i][j]
+    for i in range(len(map)):
+        for j in range(len(map[0])):
+            if tmp[i][j] == '.':
+                if tmp[i-1][j] == 'v':
+                    map_[i][j] = 'v'
+                    map_[i-1][j] = '.'
+
+def pm(map):
+    for line in map:
+        for c in line:
+            print(c, end='')
+        print()
+    print()
+
+i = 0
+while True:
+    print(i)
+    i += 1
+    step(map, map_,tmp)
+    if map == map_:
+        break
+    i += 1
+    step(map_, map,tmp)
+    if map == map_:
+        break
+
+print(f"part 1: {i}")
+
+
+        
+
+
