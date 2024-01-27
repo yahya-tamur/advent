f9e9449ee334325cccaf330c216b3c17f3aba66d
kerem <77764573+yahya-tamur@users.noreply.github.com>
2024-01-26 21:59:49 -0800

    added error message

--- extension/popup.js
+++ extension/popup.js
@@ -3,10 +3,13 @@ document.getElementById("submit").onclick = async () => {
     const cs = await chrome.cookies.getAll({});
     let session = cs.find((c) => c.name == "session").value;
     let command = `python -c "
-import os
-while 'session.txt' not in os.listdir():
+import os, sys
+for _ in range(10):
+    if 'session.txt' in os.listdir():
+        open('session.txt', 'w').write('#session#')
+        sys.exit()
     os.chdir('..')
-open('session.txt', 'w').write('#session#')
+print('Couldn\\\'t find a session.txt file. Did you run setup-downloads.py?')
 "
 
 `.replace("#session#", session)
