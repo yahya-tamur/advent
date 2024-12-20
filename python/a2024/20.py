5ebd178f86b238e3ce16fbd1d003e8d9dfc6fef5
kerem <77764573+yahya-tamur@users.noreply.github.com>
2024-12-20 11:17:14 -0800

    fastest 20

--- python/a2024/20.py
+++ python/a2024/20.py
@@ -1,5 +1,6 @@
 from problem import get_problem, get_problem_lines, look
 from collections import defaultdict, deque
+from time import time
 
 m = defaultdict(lambda:'!')
 
@@ -15,32 +16,30 @@ for i, line in enumerate(get_problem_lines()):
             case 'S': start = z
             case 'E': end = z
 
-start_ = next(start+d for d in (1, -1, 1j, -1j) if m[start+d] == '.')
-path = [start, start_]
-
+path = [start, next(start+d for d in (1,-1,1j,-1j) if m[start+d] == '.')]
 ended = False
 
 while not ended:
+    z = path[-1]
     ended = True
-    for d in (1, -1, 1j, -1j):
-        nxt = path[-1] + d
-        if nxt != path[-2] and m[nxt] == '.':
-            path.append(nxt)
+    for z_ in (z+1, z-1, z+1j, z-1j):
+        if z_ != path[-2] and m[z_] == '.':
+            path.append(z_)
             ended = False
             break
+
 
 def man(z, z_):
     return int(abs(z.real - z_.real) + abs(z.imag - z_.imag))
 
-cheats1 = [0 for _ in range(len(m))]
-cheats2 = [0 for _ in range(len(m))]
+ans1, ans2 = 0, 0
 
 for i in range(len(path)):
     for j in range(i+2, len(path)):
-        if (m := man(path[i], path[j])) <= 2:
-            cheats1[j - i - m] += 1
-        if (m := man(path[i], path[j])) <= 20:
-            cheats2[j - i - m] += 1
+        if j - i - (l := man(path[i], path[j])) < 100:
+            continue
+        ans1 += l <= 2
+        ans2 += l <= 20
 
-print(f"part 1: {sum(cheats1[100:])}")
-print(f"part 2: {sum(cheats2[100:])}")
+print(f"part 1: {ans1}")
+print(f"part 2: {ans2}")
