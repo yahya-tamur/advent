a65618bdeef161315a4d5782907949d76e32d250
kerem <77764573+yahya-tamur@users.noreply.github.com>
2025-12-07 22:09:17 -0800

    implemented union find but it made the program slightly slower??

--- python/a2025/08.py
+++ python/a2025/08.py
@@ -1,6 +1,8 @@
 from problem import get_problem, get_problem_lines, look
 from heapq import nlargest
+from time import time
 
+AAA = time()
 def e_sq(a, b):
     a1, a2, a3 = a
     b1, b2, b3 = b
@@ -11,33 +13,42 @@ dots = [eval(line) for line in get_problem_lines()]
 
 # 1000 x 1000 is ok actually
 
-connections = [(i, j) for j in range(1000) for i in range(j)]
+connections = [(i, j) for j in range(len(dots)) for i in range(j)]
 connections.sort(key=(lambda ij: e_sq(dots[ij[0]], dots[ij[1]])))
 
-# group id dot i belogns to
-groups = [i for i in range(len(dots))]
-# dots belonging to group id i
-cogroups = [[i] for i in range(len(dots))]
 
-group_sizes = [1 for i in range(len(dots))]
+# group id dot i belogns to
+groups = list(range(len(dots)))
+sizes = [1 for _ in range(len(dots))]
 full_groups = len(dots)
+
+def find(i):
+    while groups[i] != i:
+        groups[i] = groups[groups[i]]
+        i = groups[i]
+    return i
+
+
+def union(i, j):
+    i, j = find(i), find(j)
+    if i == j:
+        return False
+    if sizes[i] < sizes[j]:
+        i, j = j, i
+    groups[j] = i
+    sizes[i] += sizes[j]
+    sizes[j] = 0
+    return True
+
 
 for n, (i, j) in enumerate(connections):
     if n == 1000:
-        a, b, c = nlargest(3, group_sizes)
+        a, b, c = nlargest(3, sizes)
         print(f"part 1: {a*b*c}")
-    if j < i:
-        i, j = j, i
-    # not fully optimized (wikipedia 'disjoint-set data structure')
-    # but n=1000 here
-    if (gi := groups[i]) != (gj := groups[j]):
-        group_sizes[gi] += group_sizes[gj]
-        group_sizes[gj] = 0
-        cogroups[gi] += cogroups[gj]
-        for k in cogroups[gj]:
-            groups[k] = gi
-        cogroups[gj].clear()
+    gi, gj = groups[i], groups[j]
+    if union(gi,gj):
         full_groups -= 1
         if full_groups == 1:
             print(f"part 2: {dots[i][0]*dots[j][0]}")
-            exit()
+            print(time() - AAA)
+            break
